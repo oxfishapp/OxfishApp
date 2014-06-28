24041ba1eabd0438384915b00679fe5eb4aaad92
Emerson <root@emerson-N53SN>
2014-06-27 22:36:48 -0500

    DEBUG

--- src/application.py
+++ src/application.py
@@ -18,17 +18,17 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 SECRET_KEY_ANONYMOUS = os.environ.get('SECRET_KEY_ANONYMOUS')
 OX_TOKEN_USER_LIFETIME = int(os.environ.get('OX_TOKEN_USER_LIFETIME'))
 OX_TOKEN_GUEST_USER_LIFETIME = int(os.environ.get('OX_TOKEN_GUEST_USER_LIFETIME'))
-FLASK_DEBUG = 'false' if os.environ.get('FLASK_DEBUG') is None else os.environ.get('FLASK_DEBUG')
+#FLASK_DEBUG = 'false' if os.environ.get('FLASK_DEBUG') is None else os.environ.get('FLASK_DEBUG')
 
 application = Flask(__name__)
 application.config.from_object(__name__)
 
 # Only enable Flask debugging if an env var is set to true
-application.debug = application.config['FLASK_DEBUG'] in ['true', 'True']
+#application.debug = application.config['FLASK_DEBUG'] in ['true', 'True']
 
 #registrar los blueprints en la aplicacion
 application.register_blueprint(endpoints)
 #application.register_blueprint(errors)
 
 if __name__ == "__main__":
-    application.run(host='0.0.0.0')
+    application.run(host='0.0.0.0',debug=True)
